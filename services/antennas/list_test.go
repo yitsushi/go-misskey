9b3415ce2543194e40178d6a9dc0fedb11d27fde
Balazs Nadasdi <balazs.nadasdi@cheppers.com>
2020-10-12 15:57:06 +0200

    add examples

---
+++ services/antennas/list_test.go
@@ -0,0 +1,22 @@
+package antennas_test
+
+import (
+	"log"
+	"os"
+
+	"github.com/yitsushi/go-misskey"
+)
+
+func ExampleService_List() {
+	client := misskey.NewClient("https://slippy.xyz", os.Getenv("MISSKEY_TOKEN"))
+
+	antennas, err := client.Antennas().List()
+	if err != nil {
+		log.Printf("[Antennas/List] %s", err)
+		return
+	}
+
+	for _, antenna := range antennas {
+		log.Printf("[Antennas/List] %s created", antenna.Name)
+	}
+}
