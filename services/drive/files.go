38d78087216832234a455653e06f4f20f7abe375
Efertone <efertone@pm.me>
2020-10-21 14:05:11 +0200

    With v12.49.0 /drive/files is working as intended

--- services/drive/files.go
+++ services/drive/files.go
@@ -14,21 +14,6 @@ type FilesRequest struct {
 	Type     core.String `json:"type"`
 }
 
-// FilesRequestWithoutType is the same as FilesRequest, but without the Type
-// field. The reason is simple:
-// This field is marked as optional and nullable on the endpoint, but has no default
-// value defined like folderID has, therefore, even if it's nullable, if it's defined
-// as null, the endpoint throws back an error, that it can't be null.
-// Endpoint reference:
-// https://github.com/syuilo/misskey/blob/develop/src/server/api/endpoints/drive/files.ts#L39
-type FilesRequestWithoutType struct {
-	Limit    uint        `json:"limit"`
-	SinceID  string      `json:"sinceId"`
-	UntilID  string      `json:"untilId"`
-	FolderID core.String `json:"folderId"`
-	Type     core.String `json:"type"`
-}
-
 // FilesOptions holds all values that can be passed as a parameter for FileRequest.
 type FilesOptions struct {
 	Limit    uint
