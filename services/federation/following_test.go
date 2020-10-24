936ff7f3ad3e07cecc3c7abc19aea37e1ea0c671
Gergely Brautigam <gergely@arangodb.com>
2020-10-24 08:55:45 +0200

    Added following example

---
+++ services/federation/following_test.go
@@ -0,0 +1,24 @@
+package federation_test
+
+import (
+	"log"
+	"os"
+
+	"github.com/yitsushi/go-misskey"
+	"github.com/yitsushi/go-misskey/services/federation"
+)
+
+func ExampleService_Following() {
+	client := misskey.NewClient("https://slippy.xyz", os.Getenv("MISSKEY_TOKEN"))
+
+	resp, err := client.Federation().Following(federation.FollowingOptions{
+		Limit: 100,
+	})
+	if err != nil {
+		log.Printf("[Federation/Following] %s", err)
+
+		return
+	}
+
+	log.Printf("[Federation/Following] %v listed", resp)
+}
