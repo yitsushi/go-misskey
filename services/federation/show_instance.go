e5b31aceeba699c7209f9beffd701d94c6f8c68f
Gergely Brautigam <gergely@arangodb.com>
2020-10-26 07:56:22 +0100

    Added show instance to federation

---
+++ services/federation/show_instance.go
@@ -0,0 +1,35 @@
+package federation
+
+import (
+	"github.com/yitsushi/go-misskey/core"
+	"github.com/yitsushi/go-misskey/models"
+)
+
+// ShowInstanceRequest contains request information to obtain a single instance.
+type ShowInstanceRequest struct {
+	Host string `json:"host"`
+}
+
+// Validate the request.
+func (r *ShowInstanceRequest) Validate() error {
+	if r.Host == "" {
+		return core.RequestValidationError{
+			Request: r,
+			Message: core.UndefinedRequiredField,
+			Field:   "Host",
+		}
+	}
+	return nil
+}
+
+// ShowInstance returns a single instance given a host.
+func (s *Service) ShowInstance(request ShowInstanceRequest) (models.Instance, error) {
+	var response models.Instance
+
+	err := s.Call(
+		&core.JSONRequest{Request: &request, Path: "/federation/show-instance"},
+		&response,
+	)
+
+	return response, err
+}
---
+++ services/federation/show_instance_test.go
@@ -0,0 +1,66 @@
+package federation_test
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/yitsushi/go-misskey"
+	"github.com/yitsushi/go-misskey/services/federation"
+	"github.com/yitsushi/go-misskey/test"
+)
+
+func TestService_ShowInstance(t *testing.T) {
+	mockClient := test.SimpleMockEndpoint(&test.SimpleMockOptions{
+		Endpoint:     "/api/federation/show-instance",
+		RequestData:  &federation.ShowInstanceRequest{},
+		ResponseFile: "show_instance.json",
+		StatusCode:   http.StatusOK,
+	})
+
+	client := misskey.NewClient("https://localhost", "thisistoken")
+	client.HTTPClient = mockClient
+
+	instance, err := client.Federation().ShowInstance(federation.ShowInstanceRequest{
+		Host: "slippy.xyz",
+	})
+	if !assert.NoError(t, err) {
+		return
+	}
+
+	assert.NotEmpty(t, instance)
+	// Do a random check to ensure marshalling went okay.
+	assert.Equal(t, "12.50.0", instance.SoftwareVersion)
+}
+
+func TestShowInstanceRequest_Validate(t *testing.T) {
+	testCase := federation.ShowInstanceRequest{}
+	assert.Error(t, testCase.Validate())
+
+	testCase = federation.ShowInstanceRequest{
+		Host: "slippy.xyz",
+	}
+	assert.Error(t, testCase.Validate())
+
+	testCase = federation.ShowInstanceRequest{
+		Host: "slippy.xyz",
+	}
+	assert.NoError(t, testCase.Validate())
+}
+
+func ExampleService_ShowInstance() {
+	client := misskey.NewClient("https://slippy.xyz", os.Getenv("MISSKEY_TOKEN"))
+
+	resp, err := client.Federation().ShowInstance(federation.ShowInstanceRequest{
+		Host: "slippy.xyz",
+	})
+	if err != nil {
+		log.Printf("[Federation/ShowInstance] %s", err)
+
+		return
+	}
+
+	log.Printf("[Federation/ShowInstance] %v listed", resp)
+}
