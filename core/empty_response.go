901cfcffa6d301bee1d3861b9ca1f8f42850966c
Gergely Brautigam <skarlso777@gmail.com>
2020-10-28 08:47:44 +0100

    Update core/empty_response.go
    Co-authored-by: Balazs Nadasdi <yitsushi@gmail.com>

--- core/empty_response.go
+++ core/empty_response.go
@@ -1,7 +1,7 @@
 package core
 
 // EmptyResponse is an empty struct. The main reason behind this
-// dummy response is that there are endpoints where the return
+// empty response is that there are endpoints where the return
 // body is empty, but they have 2xx response code, therefore
 // it was successful.
 type EmptyResponse struct{}
