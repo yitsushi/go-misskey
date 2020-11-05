72f86dd1ddfca21548832d1a0150654e419777a0
Efertone <efertone@pm.me>
2020-11-06 00:20:41 +0100

    children

---
+++ services/notes/children.go
@@ -0,0 +1,47 @@
+package notes
+
+import (
+	"github.com/yitsushi/go-misskey/core"
+	"github.com/yitsushi/go-misskey/models"
+)
+
+// ChildrenRequest represents an Children request.
+type ChildrenRequest struct {
+	NoteID  string `json:"noteId"`
+	Limit   uint   `json:"limit"`
+	SinceID string `json:"sinceId,omitempty"`
+	UntilID string `json:"untilId,omitempty"`
+}
+
+// Validate the request.
+func (r ChildrenRequest) Validate() error {
+	if r.NoteID == "" {
+		return core.RequestValidationError{
+			Request: r,
+			Message: core.UndefinedRequiredField,
+			Field:   "NoteID",
+		}
+	}
+
+	if r.Limit < 1 || r.Limit > 100 {
+		return core.RequestValidationError{
+			Request: r,
+			Message: core.NewRangeError(1, 100),
+			Field:   "Limit",
+		}
+	}
+
+	return nil
+}
+
+// Children endpoint.
+func (s *Service) Children(request ChildrenRequest) ([]models.Note, error) {
+	var response []models.Note
+
+	err := s.Call(
+		&core.JSONRequest{Request: &request, Path: "/notes/children"},
+		&response,
+	)
+
+	return response, err
+}
