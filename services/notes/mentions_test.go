d34fd310c03614eb3d731a691e7ca07c9a625441
Efertone <efertone@pm.me>
2020-11-05 23:58:59 +0100

    mention && unrenote

---
+++ services/notes/mentions_test.go
@@ -0,0 +1,65 @@
+package notes_test
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"testing"
+
+	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
+	"github.com/yitsushi/go-misskey"
+	"github.com/yitsushi/go-misskey/core"
+	"github.com/yitsushi/go-misskey/services/notes"
+	"github.com/yitsushi/go-misskey/test"
+)
+
+func TestService_Mentions(t *testing.T) {
+	client := test.MakeMockClient(test.SimpleMockOptions{
+		Endpoint:     "/api/notes/mentions",
+		RequestData:  &notes.MentionsRequest{},
+		ResponseFile: "mentions.json",
+		StatusCode:   http.StatusOK,
+	})
+
+	notes, err := client.Notes().Mentions(notes.MentionsRequest{
+		Limit: 3,
+	})
+	if !assert.NoError(t, err) {
+		return
+	}
+
+	assert.Len(t, notes, 3)
+}
+
+func TestMentionsRequest_Validate(t *testing.T) {
+	test.ValidateRequests(
+		t,
+		[]core.BaseRequest{
+			notes.MentionsRequest{},
+		},
+		[]core.BaseRequest{
+			notes.MentionsRequest{
+				Limit: 3,
+			},
+		},
+	)
+}
+
+func ExampleService_Mentions() {
+	client := misskey.NewClient("https://slippy.xyz", os.Getenv("MISSKEY_TOKEN"))
+	client.LogLevel(logrus.DebugLevel)
+
+	noteList, err := client.Notes().Mentions(notes.MentionsRequest{
+		Limit: 3,
+	})
+	if err != nil {
+		log.Printf("[Notes] Error happened: %s", err)
+
+		return
+	}
+
+	for _, note := range noteList {
+		log.Println(note.Text)
+	}
+}
---
+++ services/notes/unrenote_test.go
@@ -0,0 +1,40 @@
+package notes_test
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"testing"
+
+	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
+	"github.com/yitsushi/go-misskey"
+	"github.com/yitsushi/go-misskey/services/notes"
+	"github.com/yitsushi/go-misskey/test"
+)
+
+func TestService_Unrenote(t *testing.T) {
+	client := test.MakeMockClient(test.SimpleMockOptions{
+		Endpoint:     "/api/notes/unrenote",
+		RequestData:  &notes.UnrenoteRequest{},
+		ResponseFile: "empty",
+		StatusCode:   http.StatusNoContent,
+	})
+
+	err := client.Notes().Unrenote("noteid")
+	if !assert.NoError(t, err) {
+		return
+	}
+}
+
+func ExampleService_Unrenote() {
+	client := misskey.NewClient("https://slippy.xyz", os.Getenv("MISSKEY_TOKEN"))
+	client.LogLevel(logrus.DebugLevel)
+
+	err := client.Notes().Unrenote("noteid")
+	if err != nil {
+		log.Printf("[Notes] Error happened: %s", err)
+
+		return
+	}
+}
